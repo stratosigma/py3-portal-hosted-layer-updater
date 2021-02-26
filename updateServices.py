1e35b76fbd9c697a3587d2ba86333687e655118a
Nick Nolte <github@nnwx.us>
2021-02-26 10:31:01 -0600

    Added process flag
    Added process flag to indicate whether or not the current item should be processed

--- updateServices.py
+++ updateServices.py
@@ -29,6 +29,7 @@ staging = os.path.join(sys.path[0], "staging")
 arcpy.env.overwriteOutput = True
 
 Log("[INFO] {} services to update".format(str(len(services))))
+Log("[INFO] Staging {}".format(staging))
 
 if not os.path.exists(staging):
     try:
@@ -51,63 +52,66 @@ if gis:
         service_name = service["name"]
         service_type = service["type"]
         service_folder = service["portalfolder"]
+        service_process = service["process"]
         service_sharing = service["sharing"]
         service_map = service["map"]
         service_sddraft = os.path.join(staging, "{}.sddraft".format(service_name))
         service_sd = os.path.join(staging,"{}.sd".format(service_name))
-
-        try:
-            Log("[PASS] Loaded project file for {}".format(service_name))
-            project = arcpy.mp.ArcGISProject(service["project"])
-        except:
-            Log("[FAIL] Failed to load project file for {}".format(service_name))
-            project = None
-
-        if project:
+        if service_process:
             try:
-                mapview = project.listMaps(service_map)[0]
-                Log("[PASS] Retrieved map for {}".format(service_name))
+                Log("[PASS] Loaded project file for {}".format(service_name))
+                project = arcpy.mp.ArcGISProject(service["project"])
             except:
-                Log("[FAIL] Failed to retrieve map for {}".format(service_name))
-                mapview = None
+                Log("[FAIL] Failed to load project file for {}".format(service_name))
+                project = None
 
-            if mapview:
+            if project:
                 try:
-                    draft = mapview.getWebLayerSharingDraft("HOSTING_SERVER", service_type, service_name)
-                    draft.exportToSDDraft(service_sddraft)
-                    Log("[PASS] Created Draft Service Definition for {}".format(service_name))
-                    try:
-                        arcpy.StageService_server(service_sddraft, service_sd)
-                        Log("[PASS] Staged Service {}".format(service_name))
-                        try:
-                            items = gis.content.search("{} AND owner:{}".format(service_name, username), item_type="Service Definition")
-                            Log("[INFO] Searching for existing Service Definition")
-                            Log("[INFO] Found {} matching Service Definitions".format(str(len(items))))
-                            if len(items) == 1:
-                                sditem = items[0]
-                                Log("[PASS] Found existing Service Definition for {} ({})".format(service_name, sditem.id))
-                                try:
-                                    Log("[INFO] Attempting to overwrite existing Feature Service Definition for {}".format(service_name))
-                                    sditem.update(data=service_sd)
-                                    fs = sditem.publish(overwrite=True)
-                                    Log("[PASS] Successfully overwrote existing Feature Service Definition for {}".format(service_name))
-                                    try:
-                                        Log("[INFO] Updating sharing on {}".format(service_name))
-                                        fs.share(org=service_sharing["org"], everyone=service_sharing["public"], groups=service_sharing["groups"])
-                                        Log("[PASS] Updated sharing on {}".format(service_name))
-                                        Log("[PASS] Successfully processed service {}".format(service_name))
-                                    except:
-                                        Log("[FAIL] Failed to update sharing on {}".format(service_name))                                        
-                                except:
-                                    Log("[FAIL] Failed to overwrite existing Feature Service Defintion for {}".format(service_name))
-                            else:
-                                Log("[FAIL] Found more than one matching Service Definition for {}".format(service_name))
-                        except:
-                            Log("[FAIL] Failed to search for existing Service Definition")
-                    except:
-                        Log("[FAIL] Failed to Stage Service {}".format(service_name))
+                    mapview = project.listMaps(service_map)[0]
+                    Log("[PASS] Retrieved map for {}".format(service_name))
                 except:
-                    Log("[FAIL] Failed to create Draft Service Definition for {}".format(service_name))
-                    Log(arcpy.GetMessages())
+                    Log("[FAIL] Failed to retrieve map for {}".format(service_name))
+                    mapview = None
+
+                if mapview:
+                    try:
+                        draft = mapview.getWebLayerSharingDraft("HOSTING_SERVER", service_type, service_name)
+                        draft.exportToSDDraft(service_sddraft)
+                        Log("[PASS] Created Draft Service Definition for {}".format(service_name))
+                        try:
+                            arcpy.StageService_server(service_sddraft, service_sd)
+                            Log("[PASS] Staged Service {}".format(service_name))
+                            try:
+                                items = gis.content.search("{} AND owner:{}".format(service_name, username), item_type="Service Definition")
+                                Log("[INFO] Searching for existing Service Definition")
+                                Log("[INFO] Found {} matching Service Definitions".format(str(len(items))))
+                                if len(items) == 1:
+                                    sditem = items[0]
+                                    Log("[PASS] Found existing Service Definition for {} ({})".format(service_name, sditem.id))
+                                    try:
+                                        Log("[INFO] Attempting to overwrite existing Feature Service Definition for {}".format(service_name))
+                                        sditem.update(data=service_sd)
+                                        fs = sditem.publish(overwrite=True)
+                                        Log("[PASS] Successfully overwrote existing Feature Service Definition for {}".format(service_name))
+                                        try:
+                                            Log("[INFO] Updating sharing on {}".format(service_name))
+                                            fs.share(org=service_sharing["org"], everyone=service_sharing["public"], groups=service_sharing["groups"])
+                                            Log("[PASS] Updated sharing on {}".format(service_name))
+                                            Log("[PASS] Successfully processed service {}".format(service_name))
+                                        except:
+                                            Log("[FAIL] Failed to update sharing on {}".format(service_name))                                        
+                                    except:
+                                        Log("[FAIL] Failed to overwrite existing Feature Service Defintion for {}".format(service_name))
+                                else:
+                                    Log("[FAIL] Found more than one matching Service Definition for {}".format(service_name))
+                            except:
+                                Log("[FAIL] Failed to search for existing Service Definition")
+                        except:
+                            Log("[FAIL] Failed to Stage Service {}".format(service_name))
+                    except:
+                        Log("[FAIL] Failed to create Draft Service Definition for {}".format(service_name))
+                        Log(arcpy.GetMessages())
+        else:
+            Log("[INFO] Skipped {} - not flagged for processing".format(service_name))
     Log("[INFO] Completed processing services")
 Log("[INFO] DONE")
